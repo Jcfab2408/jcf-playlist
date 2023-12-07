2a670b47f065f6ff481cc5d5cddb58957828cd16
Carlos Fabian <143008790+Jcfab2408@users.noreply.github.com>
2023-12-07 02:29:13 -0500

    Update home.js

--- home.js
+++ home.js
@@ -18,7 +18,7 @@ const songs = [
 
 // Song Links
 const urlmp3 = [
-  "music/salsas/LA_SOLUCION_Abrazame_Muy_Fuerte.mp3",
+  "music/salsas/LA SOLUCION - Abrazame Muy Fuerte.mp3",
   "music/salsas/ALVARO ROD - Preso (Salsa Live Session).mp3",
   "music/salsas/SENSACION SALSERA - Con la misma moneda.mp3",
   "music/salsas/GIGANTES DE LA SALSA - El hombre que yo amo.mp3"
